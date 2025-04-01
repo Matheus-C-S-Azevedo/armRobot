11ac916747d110efb55b28686c5eaa451a37bf13
matheus <matheus.costa.s.azevedo@gmail.com>
2025-04-01 18:30:32 -0400

    feat: Adicionando configuração para permitir o uso de versões não suportadas do chip do esp32
    foi adicionado um comando no arquivo de configuração 'prj.conf', este comando permite o uso de versões não suportadas do chip do esp32, este comando visa permitir o uso dos esp do outros
    integrantes do time

--- src/main.c
+++ src/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LED_PIN 2      // Pino do LED
 
 int main(void)
 {
+    const struct device *gpio = DEVICE_DT_GET(DT_NODELABEL(gpio0));    
+    // Configura o pino do LED como saída
+    gpio_pin_configure(gpio, LED_PIN, GPIO_OUTPUT_INACTIVE);
+
 	printf("Configuração inicial do projeto arm robot");
+
+	while(1){
+		
+        gpio_pin_set(gpio, LED_PIN, 1);  // Acende o LED
+		k_msleep(1000);
+        gpio_pin_set(gpio, LED_PIN, 0);  // Apaga o LED
+        k_msleep(1000);  // Pequeno delay para debounce
+	}
+
 	return 0;
 }
